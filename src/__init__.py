0fd68b70761ffbf4aecef967bc895d7ecabc5e37
msbrm <cuihaniss@gmail.com>
2024-05-23 09:01:40 +0800

    fix: add seconds2hms in abcutil

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,4 @@
 from abcdict import AbcDict
+
+from abcutil import get_now_str
+from abcutil import seconds_to_hms
--- src/abcutil.py
+++ src/abcutil.py
@@ -8,10 +8,10 @@ def get_now_str(format='%Y%m%d%H%M%S') -> str:
 def isTrue(obj):
     pass
 
-def seconds_to_hms(seconds):
+
+def seconds2hms(seconds):
     hours = seconds // 3600
     seconds %= 3600
     minutes = seconds // 60
     seconds %= 60
     return int(hours), int(minutes), int(seconds)
-    
